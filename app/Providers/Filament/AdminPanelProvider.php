923d93f005e86e7e43f76872f9c72c56548a0c49
giorgi18806 <giorgi18806@gmail.com>
2025-03-19 17:57:50 +0000

    Add Department and Category model, migrations and seeders

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -14,6 +14,7 @@ use Filament\Support\Colors\Color;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
@@ -53,9 +54,14 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
                 'auth',
                 sprintf('role:%s|%s', RolesEnum::Admin->value, RolesEnum::Vendor->value)
-            ])
+            ]);
             // ->authMiddleware([
             //     Authenticate::class,
-            ]);
+            // ]);
+    }
+
+    public function boot()
+    {
+        Model::unguard();
     }
 }
